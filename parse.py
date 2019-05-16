be6d58da60c37f43ce0bcd2f8001b7d19e730327
Kiryl Lepchankou <k.lepchenkov@gmail.com>
2019-05-16 11:21:36 +0300

    Fixed variable names, made get_soup and get_url methods private.

--- parse.py
+++ parse.py
@@ -11,11 +11,11 @@ from db_connect import Postgres_db
 ##########################class_implementation########################
 class Parser(object):
 
-    def get_url(self, url):
+    def _get_url(self, url):
         return get(url)
 
-    def get_soup(self, url):
-        content  = self.get_url(url)
+    def _get_soup(self, url):
+        content  = self._get_url(url)
         soup = BSoup(content.text, 'html.parser')
         return soup
 
@@ -29,7 +29,7 @@ class Parser(object):
 
     def _get_category_obj(self):
         url = self._construct_url('/catalog')
-        soup = self.get_soup(url)
+        soup = self._get_soup(url)
         objs = soup.findAll('section',\
                             {'class':\
                              'bordered-section js-accordion-group'})
@@ -47,41 +47,41 @@ class Parser(object):
         return name
 
     def _get_subcat_lvl1_objects(self):
-        for cat_obj, cat_name in self._get_category_obj():
-            subcat_lvl1_objs = cat_obj.findAll('div',\
+        for category_obj, category in self._get_category_obj():
+            subcat_lvl1_objs = category_obj.findAll('div',\
                                               {'class':'catalog-all-item'})
             for obj in subcat_lvl1_objs:
                 name = self._get_subcat_lvl1_name(obj)
-                p_name = cat_name
-                yield obj, name, p_name
+                parent = category
+                yield obj, name, parent
 
     def get_lvl1_subcategories(self):
-        for _, name, p_name in self._get_subcat_lvl1_objects():
-            yield name, p_name
+        for _, name, parent in self._get_subcat_lvl1_objects():
+            yield name, parent
 
     def _get_subcat_lvl2_objects(self):
-        for subcat_lvl1_obj, s_lvl1_name, cat_name  in self._get_subcat_lvl1_objects():
+        for subcat_lvl1_obj, s_lvl1_name, cat_name in self._get_subcat_lvl1_objects():
             objs = subcat_lvl1_obj.findAll('a',\
                                           {'class':'section-submenu-sublink'})
             for obj in objs:
                 name = obj.get_text()
                 url = self._construct_url(obj.get('href'))
-                p_name = s_lvl1_name
-                g_p_name = cat_name
-                yield obj, name, p_name, g_p_name, url
+                parent = s_lvl1_name
+                grandparent = cat_name
+                yield obj, name, parent, grandparent, url
 
     def get_lvl2_subcategories(self):
-        for _, name, p_name, g_p_name, url in self._get_subcat_lvl2_objects():
-            yield name, p_name, g_p_name, url
+        for _, name, parent, grandparent, url in self._get_subcat_lvl2_objects():
+            yield name, parent, grandparent, url
 
     def _get_subpage_urls(self, subcat_lvl2_url):
-        soup = self.get_soup(subcat_lvl2_url)
+        soup = self._get_soup(subcat_lvl2_url)
         button_combo_object = soup.select('div.btn-combo div.hide a')
         for a_tag in button_combo_object:
             yield self._construct_url(a_tag.attrs["href"])
 
     def _extract_product_link(self, page_url):
-        soup = self.get_soup(page_url)
+        soup = self._get_soup(page_url)
         product_cards = soup.select('div.catalog-grid div.product-item_img-box')
         for card in product_cards:
             url_raw = card.select('a.no-border-product')
@@ -89,7 +89,7 @@ class Parser(object):
             yield url
 
     def get_product_link(self):
-        for name, p_name, g_p_name, url in self.get_lvl2_subcategories():
+        for name, parent, grandparent, url in self.get_lvl2_subcategories():
             for subpage_url in self._get_subpage_urls(url):
                 for url in self._extract_product_link(subpage_url):
-                    yield url, name, p_name, g_p_name
+                    yield url, name, parent, grandparent
