a278c812e83d547b89b7b8018fb302beee69c5bb
Kiryl Lepchankou <k.lepchenkov@gmail.com>
2019-05-21 19:19:14 +0300

    Reorganized test notebooks.

--- stage1_parsing.py
+++
@@ -1,6 +0,0 @@
-from downloader import Downloader
-
-
-db_config = 'postgresql://postgres:test1234@localhost:5432/oma_catalog_test'
-dwnld = Downloader(db_config, create_new_tables=True)
-dwnld.parse_main_catalog_page_single_run()
