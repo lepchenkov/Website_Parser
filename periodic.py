f0ac02598d71948b187dbe0b0e0ff92718a7ef71
Kiryl Lepchankou <k.lepchenkov@gmail.com>
2019-06-05 12:10:59 +0300

    Separated main page parsing from scheduler, moved parsing logic to single periodic.py file. The file may be executed for stage1 and stage2 parsing. For stage3 parsing Celery beat service must me activated as well as Celery worker. All necessary configurations are located in periodic.py file.

--- periodic.py
+++ periodic.py
@@ -1,17 +1,44 @@
+#!/usr/bin/env python3
 import time
+import logging
 from celery import Celery
-from oma_celery import descrete_parsing
+from downloader import Downloader
+from db_configurator import get_config_string
 
 
 app = Celery('periodic', broker='amqp://localhost//')
 
 @app.task
 def run_parsing_procedure():
-    descrete_parsing()
+    parse_products()
 
 app.conf.beat_schedule = {
-    "run_parsing": {
-        "task": "periodic.run_parsing_procedure",
-        "schedule": 2.0
+    "run_parsing_product_info": {
+        "task": "periodic.parse_products",
+        "schedule": 5.0
     }
 }
+
+def parse_products():
+    download.parse_products_parameters(number_of_products_to_parse=10)
+    return True
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='oma_parsing.log', level=logging.INFO,
+                        format='%(asctime)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.info('Started')
+    db_config = get_config_string()
+    download = Downloader(db_config, create_new_tables=False)
+    logging.info('Downloader initiated with create_new_tables=False')
+    if not download.check_if_stage1_parsing_is_complete():
+        logging.info('Stage_1 parsing started')
+        download = Downloader(db_config, create_new_tables=True)
+        logging.info('Downloader initiated without creating new tables')
+        download.parse_main_catalog_page_single_run()
+        logging.info('Stage_1 parsing is finished')
+    logging.info('Stage_2 parsing started')
+    while not download.check_if_stage2_parsing_is_complete():
+        logging.info('Successful check of incompletion of stage_2 parsing')
+        download.parse_product_url_from_subcats_lvl2(number_of_subcats=1)
+    logging.info('Stage_2 parsing finished')
